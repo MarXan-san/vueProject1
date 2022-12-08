ff99437ee94493e89338e3e2a732e8716d76c350
Maria Xanthouli <maria2007x@gmail.com>
2022-12-08 15:47:47 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<script  lang="ts">
+
+import Component1 from "@/components/Component1/Component1.vue";
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  components: {Component1},
+  data() {
+    return {
+      names: ["Maria", "Marios", "Attila", "Simeon", "Ioannis"],
+      people1: [
+        {
+          name: "Odett",
+          color: "blue"
+        },
+        {
+          name: "David",
+          color: "#f0f"
+        },
+        {
+          name: "Daniel",
+          color: "black"
+        }
+      ],
+      people2: [
+        {
+          name: "Filippos",
+          color: "green"
+        },
+        {
+          name: "Ioanna",
+          color: "red"
+        },
+        {
+          name: "Adam",
+          color: "orange"
+        }
+      ]
+    }
+  }
+})
+
+</script>
+
+<template>
+
+<Component1 :people="people1"></Component1>
+  <Component1 :people="people2"></Component1>
+
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Component1/Component1.vue
@@ -0,0 +1,36 @@
+
+<script>
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: "Component1",
+  props: {
+    people: {
+      default: 0
+    }
+  }
+
+});
+</script>
+
+<template>
+
+  <ul>
+<!--    <li v-for="name in names">{{ name }}!</li>-->
+    <li v-for="person in people" :style="{'color':person.color}">{{ person.name }}!</li>
+  </ul>
+
+</template>
+
+
+<style scoped>
+
+/*li:nth-child(odd){*/
+/*  color: burlywood;*/
+/*}*/
+
+/*li:nth-child(even){*/
+/*  color: red;*/
+/*}*/
+
+</style>
